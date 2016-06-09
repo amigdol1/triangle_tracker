427faa49cad95cca7c082690229c16406423b21c
Alex Migdol and April Peng <alex.migdol@gmail.com>
2016-06-08 19:48:03 -0700

    add margin top padding to result output in css. fix bug in js where it was forms# instead of form#

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-
-
 $(function() {
-  $("forms#triangle").submit(function() {
+  $("form#triangle").submit(function(event) {
     event.preventDefault();
     var a = parseInt($("#a").val());
+    console.log(a);
     var b = parseInt($("#b").val());
+    console.log(b);
     var c = parseInt($("#c").val());
+    console.log(c);
     var result;
     if ((a === b) && (b === c)) {
-      result = "equilateral";
+      result = "Equilateral";
     } else if (((a === b) && (a !=c)) || ((b === c) && (b != a)) || ((c === a) && (c != b))) {
-      result = "isosceles";
+      result = "Isosceles";
     } else if (((a + b) <= c) || ((b + c) <= a) || ((c + a) <= b)) {
       result = "NOT a triangle. Try again!";
     } else {
-      result = "scalene";
+      result = "Scalene";
     }
     $("#result").text(result);
   });
